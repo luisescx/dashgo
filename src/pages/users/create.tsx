2f609229ffa025537d2a244e0811a5117a63c447
luisescx <luis.esculapio@live.com>
2022-06-04 22:27:26 -0300

    fix: added mutation on create user

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -17,6 +17,10 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useCallback } from "react";
+import { useMutation } from "react-query";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
 type CreateUserFormData = {
   name: string;
@@ -35,18 +39,39 @@ const createUserFormSchema = yup.object().shape({
 });
 
 export default function CreateUser() {
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post("users", {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("users");
+      },
+    }
+  );
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema),
   });
+
+  const router = useRouter();
 
   const { errors } = formState;
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = useCallback(
     async (values) => {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-      console.log(values);
+      await createUser.mutateAsync(values);
+
+      router.push("/users");
     },
-    []
+    [createUser, router]
   );
 
   return (
